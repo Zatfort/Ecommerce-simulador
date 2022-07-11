1a4e9c7c046e342af63957365a776c724e351bbf
daniel gutierrez <daniel_gutierrezn15@hotmail.com>
2022-07-11 00:13:31 -0300

    html and css  Done

--- js/app.js
+++ js/app.js
@@ -5,3 +5,26 @@ let toggle = document.querySelector('.toggle')
 toggle.onclick = function(){
     navigation.classList.toggle('active')
 }
+
+
+// titulo animado
+let text = document.getElementById("hoo");
+let str = text.innerHTML;
+
+text.innerHTML = "";
+
+let speed = 300
+
+let i = 0
+
+function typeWriter(){
+    if(i < str.length){
+        text.innerHTML += str.charAt(i)
+        i++
+        setTimeout(typeWriter,speed)
+    }
+}
+
+setTimeout(typeWriter,speed);
+
+// titulo animado end
